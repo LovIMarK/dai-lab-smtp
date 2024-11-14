687d05cf30243fdcfa2e43fc2dd0aa28a00cfb15
heigSamFernande <samuel.fernandez@heig-vd.ch>
2024-11-14 17:18:23 +0100

    adding Skeleton

---
+++ lab-smtp/src/main/java/dai/lab/smtp/App.java
@@ -0,0 +1,10 @@
+package dai.lab.smtp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ lab-smtp/src/test/java/dai/lab/smtp/ConfigurationManagerTest.java
@@ -0,0 +1,19 @@
+package dai.lab.smtp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ConfigurationManagerTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
