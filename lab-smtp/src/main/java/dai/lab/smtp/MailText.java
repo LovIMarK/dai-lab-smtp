061e9f0701a01db5cc53887fd0b83d2b8fd729a1
heigSamFernande <samuel.fernandez@heig-vd.ch>
2024-11-18 16:41:44 +0100

    Starting mailText class

--- lab-smtp/src/main/java/dai/lab/smtp/MailText.java
+++ lab-smtp/src/main/java/dai/lab/smtp/MailText.java
@@ -1,5 +1,50 @@
 package dai.lab.smtp;
 
-public class MailText {
+import java.util.ArrayList;
+
+public class MailText 
+{
+    private static ArrayList<String> mails;
+    private Mail mail;
+
+    public MailText(String filePath)
+    {
+        if(mails == null)
+            mails = loadMessages(filePath);
+
+        
+    }
+
+    public Mail getMail()
+    {
+        return this.mail;
+    }
+
+    public static ArrayList<String> loadMessages(String filePath)
+    {
+        return null;
+    }
+
+    class Mail
+    {
+        private String header;
+        private String body;
+
+        public Mail(String header, String body)
+        {
+            this.header = header;
+            this.body = body;
+        }
+
+        public String getHeader()
+        {
+            return this.header;
+        }
+
+        public String getBody()
+        {
+            return this.body;
+        }
+    }
     
 }
